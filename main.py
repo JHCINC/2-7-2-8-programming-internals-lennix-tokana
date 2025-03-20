81c206924fbedc4dd97ddaa7c063092eed681b7d
LennixT <20159@jhc.school.nz>
2025-03-20 13:32:27 +1300

    Added files via upload - Pycharm Psuedo code

---
+++ main.py
@@ -0,0 +1,5 @@
+"""
+Input the type you want i.g {type, gender} [type = fantasy, sci-fi, modern, Medieval, etc] [gender = Male, Female, NB, ambiguous, helicopter, fem-boy]
+last name typing ina last name as a last name
+amount = amount [I want 49837 names of the sci-fi type and all female]
+"""
