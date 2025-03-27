2597e91c17577ce7e5ab8b911591afe4dfc37b1e
LennixT <20159@jhc.school.nz>
2025-03-28 09:14:03 +1300

    Added strings of code

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 """
 Input the type you want i.g {type, gender} [type = fantasy, sci-fi, modern, Medieval, etc] [gender = Male, Female, NB, ambiguous, helicopter, fem-boy]
 last name typing ina last name as a last name
-amount = amount [I want 49837 names of the sci-fi type and all female]
+amount = amount [I want 10 names of the sci-fi type and all female]
+check if the input is valid
+use a random number to select a random name from the multidimensional list with all the input types you want. Multidimensional
+ ex: [namelist =
+	["sci-fi"], - 0
+	["Fantasy"], - 1
+	["Medieval"], - 2
+	["Modern"], - 3
+blah, blah gender, blah, random num, blah]
+print the full thing out.
+If you so desire to have more use [loop, multiple names] prints more names, types, random crap.
+
 """
+import random
