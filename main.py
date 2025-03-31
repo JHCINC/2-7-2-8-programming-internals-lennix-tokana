9af001608b0a53a0c5ede30719d39e19bcc4f126
LennixT <20159@jhc.school.nz>
2025-03-31 13:31:13 +1300

    Created two categories
    Next lesson gonna do more.

--- main.py
+++ main.py
@@ -15,3 +15,30 @@ If you so desire to have more use [loop, multiple names] prints more names, type
 
 """
 import random
+namelist = {
+    "Modern": {
+        "Male": ["James", "John", "Michael", "David", "Benjamin", "Nathan", "Ryan", "Ethan", "Jack", "Leo", "Adam", "Julian", "Titus", "Elliot", "Milo"],
+        "Female": ["Emma", "Olivia", "Ava", "Sophia", "Isabella", "Mia", "Amelia", "Harper", "Evelyn", "Abigail", "Ella", "Chloe", "Lily", "Charlotte", "Grace"],
+        "Non Binary": ["Alex", "Jordan", "Taylor", "Morgan", "Riley", "Avery", "Casey", "Rowan", "Skyler", "Quinn", "Emerson", "Sage", "Cameron", "Finley", "Dakota"],
+        "Helicopter": ["Apache", "Black Hawk", "Chinook", "Huey", "Sea Hawk", "Viper", "Osprey", "Merlin", "Gazelle", "Kiowa"],
+        "Femboy": ["Lennix", "Micheal", "Ethan", "Louis", "Lachlan", "Gage"],
+
+    },
+
+    "Fantasy": {
+        "Male": ["Percival", "Merlin", "Thaddeus", "Kaelen", "Thorian", "Lysander", "Draven", "Gideon", "Zephyros", "Rylan", "Eldrin", "Vesper", "Caspian", "Nokk"],
+        "Female": ["Lyra", "Seraphina", "Isolde", "Anya", "Briar", "Elara", "Zephyra", "Rhiannon", "Valkyrie", "Calista", "Elowen", "Miriam", "Aurelia", "Sylvia", "Nimue"],
+        "Non Binary": ["Zephyr", "Rune", "Ash", "River", "Sky", "Ember", "Vale", "Rowan", "Sage", "Briar", "Cyan", "Jett", "Fable", "Lux", "Nyx"],
+
+
+
+
+
+
+
+
+    }
+
+
+
+}
